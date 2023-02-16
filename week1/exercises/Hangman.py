6126f7abdcf2aaeb76a36b1b2981eaaca4394257
elena perez <ejperezk@gmail.com>
2023-02-16 10:01:39 +0100

    committing to save changes

--- week1/exercises/Hangman.py
+++ week1/exercises/Hangman.py
@@ -3,41 +3,61 @@ The hangman game is a word guessing game where the player is given a word and ha
 The player is given a certain number of tries (no more than 6 wrong guesses are allowed) to guess the correct letters before the game is over.
 """
 
-def hangman():
-    """A simple hangman game where a letter is taken as the input and no more than 6 wrong guesses are allowed. The output is the complete word"""
-    word = "java"
-    hidden_word = "_ " * len(word)
-    used_letters = []
-    guesses = 1
 
-    for letter in word:
-        guess = input("Guess a letter: ")
-        used_letters.append(guess)
-        if guess in word:
-            # hidden_word.update()  # This is the next step I am on, update the hidden_word
-            print(f"You have {guesses} tries left.")
-            print(" ".join(used_letters))
-            print(hidden_word)
-        else:
-            guesses -= 1
-            print(f"You have {guesses} tries left.")
-            print(" ".join(used_letters))
-            if guesses == 0:
-                print(f"You have ran out of guesses, you lose :(")
-                break
-    # print(hidden_word)
+# def start_ hangman():
+#     """A simple hangman game where a letter is taken as the input and no more than 6 wrong guesses are allowed. The output is the complete word"""
+# hidden_word = input("Player 1 pick a word: ")
+hidden_word = "java"
+display_word = "_ " * len(hidden_word)
+used_letters = []
+guesses = 2
 
+while guesses > 0 and display_word != hidden_word:
+    print(display_word)
+    print(f"You have {guesses} tries left.")
+    guessed_letter = input("Guess a letter: ")
+    used_letters.append(guessed_letter)
 
-hangman()
+    if len(guessed_letter) != 1:
+        print("Please enter only one letter.")
+
+    if guessed_letter is not guessed_letter.isalpha():
+        print("Please enter a valid letter of the alphabet")
+
+    if guessed_letter in used_letters:
+        print("You have already guessed that word")
+
+    if guessed_letter in hidden_word:
+        for i in range(len(hidden_word)):
+            if hidden_word[i] == guessed_letter:
+                display_word = display_word[:1] + guessed_letter + hidden_word[i + 1 :]
+    else:
+        print("Wrong letter, try again.")
+        guesses -= 1
+
+    # else:
+    #     guesses -= 1
+    #     print(f"You have {guesses} tries left.")
+    #     print(" ".join(used_letters))
+    #     if guesses == 0:
+    #         print(f"You have ran out of guesses, you lose :(")
+    #         break
+
+# for i in range(len(hidden_word)):
+#     if hidden_word[i] == guess:
+#         display_word = display_word[:i] + guess + display_word[i + 1 :]
+# print(display_word)
+# for i in range(len(hidden_word)):
+#     if hidden_word[i] == guess:
+#         display_word = display_word.replace("_", guess, 1)
+# print(display_word)
+
+# print(hidden_word)
+
 
 # You have already used the letter {guess}, try again
 # Congratulations! You won!
 # replace() the blanks of the word
-
-
-
-
-
 
 
 # Output
