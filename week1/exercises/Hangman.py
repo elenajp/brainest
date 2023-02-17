c1cc3d9bca17e586468407ad102dd88cf9b49eb6
elena perez <ejperezk@gmail.com>
2023-02-17 14:01:33 +0100

    hangman game complete

--- week1/exercises/Hangman.py
+++ week1/exercises/Hangman.py
@@ -1,22 +1,20 @@
-"""
-The hangman game is a word guessing game where the player is given a word and has to guess the letters that make up the word. 
-The player is given a certain number of tries (no more than 6 wrong guesses are allowed) to guess the correct letters before the game is over.
-"""
-
-
-# def start_ hangman():
-#     """A simple hangman game where a letter is taken as the input and no more than 6 wrong guesses are allowed. The output is the complete word"""
-# hidden_word = input("Player 1 pick a word: ")
-hidden_word = "java"
+hidden_word = input("Player 1 enter a word: ")
 display_word = "_ " * len(hidden_word)
 used_letters = []
-guesses = 2
+guesses = 6
 
 while guesses > 0 and display_word != hidden_word:
-    print(display_word)
-    print(f"You have {guesses} tries left.")
-    guessed_letter = input("Guess a letter: ")
-    used_letters.append(guessed_letter)
+
+    print(
+        " ".join([letter if letter in used_letters else "_" for letter in hidden_word])
+    )
+    print()
+
+    guessed_letter = input("Player 2 guess a letter: ")
+
+    if "".join(used_letters) == hidden_word:
+        print("Congratulations, you guessed the word!")
+        break
 
     if len(guessed_letter) != 1:
         print("Please enter only one letter.")
@@ -25,56 +23,17 @@ while guesses > 0 and display_word != hidden_word:
         print("Please enter a valid letter of the alphabet")
 
     if guessed_letter in used_letters:
-        print("You have already guessed that word")
+        print("You already guessed that letter.")
+        continue
+
+    used_letters.append(guessed_letter)
 
     if guessed_letter in hidden_word:
-        for i in range(len(hidden_word)):
-            if hidden_word[i] == guessed_letter:
-                display_word = display_word[:1] + guessed_letter + hidden_word[i + 1 :]
+        print("Good guess!")
     else:
-        print("Wrong letter, try again.")
         guesses -= 1
+        print("Sorry, that letter is not in the word.")
 
-    # else:
-    #     guesses -= 1
-    #     print(f"You have {guesses} tries left.")
-    #     print(" ".join(used_letters))
-    #     if guesses == 0:
-    #         print(f"You have ran out of guesses, you lose :(")
-    #         break
-
-# for i in range(len(hidden_word)):
-#     if hidden_word[i] == guess:
-#         display_word = display_word[:i] + guess + display_word[i + 1 :]
-# print(display_word)
-# for i in range(len(hidden_word)):
-#     if hidden_word[i] == guess:
-#         display_word = display_word.replace("_", guess, 1)
-# print(display_word)
-
-# print(hidden_word)
-
-
-# You have already used the letter {guess}, try again
-# Congratulations! You won!
-# replace() the blanks of the word
-
-
-# Output
-"""
-You have 6 tries left.                                                                                                                                           
-Used letters:                                                                                                                                                    
-Word: _ _ _ _                                                                                                                                                    
-Guess a letter: a 
-
-You have 6 tries left.                                                                                                                                           
-Used letters: a                                                                                                                                                  
-Word: _ a _ a                                                                                                                                                    
-Guess a letter: j    
-
-You have 6 tries left.                                                                                                                                           
-Used letters: j a                                                                                                                                                
-Word: j a _ a                                                                                                                                                    
-Guess a letter: v                                                                                                                                                
-You guessed the word java !
-"""
+        if guesses == 0:
+            print("Sorry, you ran out of guesses. The word was", hidden_word)
+            break
