99dc8304b643476393d8163ab459d5a7a6bf6a27
elena perez <ejperezk@gmail.com>
2023-02-21 16:30:55 +0100

    for the gui I now have the canvas with a background image and button, committing to save changes

--- week1/exercises/weather_app/gui.py
+++ week1/exercises/weather_app/gui.py
@@ -1,23 +1,45 @@
-# Use the tkinter library to create a GUI for the app, including widgets such as labels, buttons and text boxes.
+from tkinter import *
+from PIL import Image, ImageTk
 
-# Enter city: rome
-# [{'description': 'overcast clouds', 'icon': '04d', 'id': 804, 'main': 'Clouds'}]
+root = Tk()
 
-import tkinter as tk
+image = Image.open("background.jpg")
+photo = ImageTk.PhotoImage(image)
+width, height = image.size
 
-# from weather_app import get_current_weather
+canvas = Canvas(root, width=width, height=height)
+canvas.create_image(0, 0, image=photo, anchor="nw", tags="bg")
 
-root = tk.Tk()
-root.geometry("500x500")  # how to make it change with the screen size
-root.title("Weather App")
 
-label = tk.Label(root, text="Hello World!", font=("Arial", 18))
-label.pack(padx=20, pady=20)
+def resize_image(event):
+    global photo, resized_image
+    new_width = event.width
+    new_height = event.height
+    resized_image = image.resize((new_width, new_height))
+    photo = ImageTk.PhotoImage(resized_image)
+    canvas.itemconfig("bg", image=photo)
 
-textbox = tk.Text(root, height=3, font=("Arial", 16))
-textbox.pack(padx=10, pady=10)
 
-button = tk.Button(root, text="Enter")
+canvas.bind("<Configure>", resize_image)
+
+label = Label(canvas, text="Enter a city:", font=("Arial", 18))
+canvas.create_window(
+    root.winfo_screenwidth() / 2, root.winfo_screenheight() / 2, window=label
+)
+# label.pack(padx=20, pady=20)
+
+button = Button(root, text="Enter", activebackground="teal")
 button.pack(padx=10, pady=10)
 
+# textbox = tk.Text(root, height=1, font=("Arial", 16), width=20)
+# textbox.pack(padx=10, pady=10)
+
+# city_input = tk.Entry(
+#     root,
+#     textvariable="city_value",
+#     width=24,
+#     font=("Arial", 16),
+# )  # bg="#404040". fg="white"
+
+canvas.pack()
 root.mainloop()
